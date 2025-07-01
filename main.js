03008259e4b0cd31ab8e23d41d60444015f7ffea
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-07-01 08:15:57 -0300

    jk

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
-        for (let j =0; j< botoes.length; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -14,15 +13,16 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2029-12-30T00:00:00");
-const tempoObjetivo2 = new Date("2029-12-20T00:00:00");
-const tempoObjetivo3 = new Date("2029-12-25T00:00:00");
-const tempoObjetivo4 = new Date("2029-12-26T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
+const tempoObjetivo2 = new Date("2026-01-02T00:00:00");
+const tempoObjetivo3 = new Date("2025-07-15T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-25T00:00:00");
 
-const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculatTempo(tempoObjetivo){
+function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
@@ -30,11 +30,11 @@ function calculatTempo(tempoObjetivo){
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
 
-    segundos %= 60;
-    minutos %= 60; 
-    horas %= 60;
+segundos %= 60;
+minutos %=60;
+horas %=60;
 
-    return(dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos")
+return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
 }
 
 function atualizaCronometro(){
