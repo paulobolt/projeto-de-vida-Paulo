ef494b90795cade006080a0ee983704ac4c27209
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-07-01 08:20:56 -0300

    y

--- main.js
+++ main.js
@@ -34,7 +34,13 @@ segundos %= 60;
 minutos %=60;
 horas %=60;
 
-return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+
+if(tempoFinal>0){
+    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+
+}else{
+    return "Prazo Finalizado"
+}
 }
 
 function atualizaCronometro(){
